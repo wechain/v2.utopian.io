bb58fd295e7dc1f687777252f5cc47f114a8d971
Ícaro Harry <me@icaro.io>
2018-08-14 20:48:08 -0300

    Changes in components locations

--- src/pages/crud-project/crud-project.vue
+++ src/pages/crud-project/crud-project.vue
@@ -1,7 +1,9 @@
 <script>
 // imports.
 import ULayoutPage from 'src/layouts/parts/page/page'
-import UFileUploader from 'src/components/project/file-uploader/file-uploader'
+import UFileUploader from 'src/components/form/file-uploader'
+import USelectLicense from 'src/components/form/select-license'
+
 import * as GitHub from '@octokit/rest'
 import { required, minLength } from 'vuelidate/lib/validators'
 import { mapGetters, mapActions } from 'vuex'
@@ -17,7 +19,8 @@ export default {
   // child components.
   components: {
     ULayoutPage,
-    UFileUploader
+    UFileUploader,
+    USelectLicense
   },
 
   // component data.
@@ -28,42 +31,6 @@ export default {
         'account',
         'username'
       ]),
-
-      licenseOptions: [
-        { label: 'Academic Free License v3.0', value: 'afl-3.0' },
-        { label: 'Apache license 2.0', value: 'apache-2.0' },
-        { label: 'Artistic license 2.0', value: 'artistic-2.0' },
-        { label: 'Boost Software License 1.0', value: 'bs1-1.0' },
-        { label: 'BSD 2-clause "Simplified" license', value: 'bsd-2-clause' },
-        { label: 'BSD 3-clause "New" or "Revised" license', value: 'bsd-3-clause' },
-        { label: 'BSD 3-clause Clear license', value: 'bsd-3-clause-clear' },
-        { label: 'Creative Commons license family', value: 'cc' },
-        { label: 'Creative Commons Zero v1.0 Universal', value: 'cc0-1.0' },
-        { label: 'Creative Commons Attribution 4.0', value: 'cc-by-4.0' },
-        { label: 'Creative Commons Attribution Share Alike 4.0', value: 'cc-by-sa-4.0' },
-        { label: 'Do What The F*ck You Want To Public License', value: 'wtfpl' },
-        { label: 'Educational Community License v2.0', value: 'ecl-2.0' },
-        { label: 'Eclipse Public License 1.0', value: 'epl-1.0' },
-        { label: 'European Union Public License 1.1', value: 'eupl-1.1' },
-        { label: 'GNU Affero General Public License v3.0', value: 'agpl-3.0' },
-        { label: 'GNU General Public License family', value: 'gpl' },
-        { label: 'GNU General Public License v2.0', value: 'gpl-2.0' },
-        { label: 'GNU General Public License v3.0', value: 'gpl-3.0' },
-        { label: 'GNU Lesser General Public License family', value: 'lgpl' },
-        { label: 'GNU Lesser General Public License v2.1', value: 'lgpl-2.1' },
-        { label: 'GNU Lesser General Public License v3.0', value: 'lgpl-3.0' },
-        { label: 'ISC', value: 'isc' },
-        { label: 'LaTeX Project Public License v1.3c', value: 'lppl-1.3c' },
-        { label: 'Microsoft Public License', value: 'ms-pl' },
-        { label: 'MIT', value: 'mit' },
-        { label: 'Mozilla Public License 2.0', value: 'mpl-2.0' },
-        { label: 'Open Software License 3.0', value: 'osl-3.0' },
-        { label: 'PostgreSQL License', value: 'postgresql' },
-        { label: 'SIL Open Font License 1.1', value: 'ofl-1.1' },
-        { label: 'University of Illinois/NCSA Open Source License', value: 'ncsa' },
-        { label: 'The Unlicense', value: 'unlicense' },
-        { label: 'zLib License', value: 'zlib' }
-      ],
 
       // github placeholder.
       gh: {},
