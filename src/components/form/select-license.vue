ab0176e54d20ce22adae3f9c0744530ad0398f20
Ícaro Harry <me@icaro.io>
2018-08-14 17:14:32 -0300

    Creates select-license component

---
+++ src/components/form/select-license.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  q-select(
+    label="License",
+    v-model="project.license",
+    :error="$v.project.license.$error",
+    placeholder="MIT",
+    :options="licenseOptions",
+    :before="[{ icon: 'mdi-file-outline' }]",
+    filter,
+    autofocus-filter,
+    @blur="updateFormPercentage('license')"
+  )
+</template>
+
+<script>
+import { QInput } from 'quasar'
+
+export default {
+  name: 'u-select-license',
+  components: {
+    QInput
+  },
+  props: ['onError', 'updateFormPercentage', 'readonly', 'classes', 'id'],
+  computed: {
+    allClasses () {
+      return 'u-input ' + this.classes
+    }
+  },
+  methods: {
+    handleChange (val) {
+      this.$emit('input', val)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .u-input {
+    background #fff
+    border solid 1px #e0e2e5
+    border-bottom none
+    padding-top 8px
+    padding-left 10px
+    &:before {
+      background #e0e2e5
+    }
+  }
+</style>
